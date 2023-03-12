cd559dc3de1561f76bb0f7d06fd97edb47cd8169
Duke Romkey <duke@Dukes-MacBook-Pro.local>
2023-03-11 18:14:04 -0600

    Connection test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hubby To Do</Text>
+      <Text>Hubby To Do List</Text>
       <StatusBar style="auto" />
     </View>
   );
