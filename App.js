435972636f1e4321716dfb1ca2aec3d97145a149
Duke Romkey <duke@Dukes-MacBook-Pro.local>
2023-03-11 22:45:08 -0600

    initial add goal input skeleton

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View, Pressable } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hubby To Do List</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hubby To Do List</Text>
+        {/* INPUT AREA */}
+        <View style={styles.addGoalContainer}>
+          <View style={styles.goalInput}>
+            <TextInput placeholder="Add Goal" placeholderTextColor="white" />
+            <View style={styles.addGoalButton}>
+              <Pressable>
+                <Text style={styles.buttonText}>Add Goal</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 60,
     backgroundColor: "#fff",
     alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+    padding: 16,
+  },
+  goalInput: {
+    position: "relative",
+    padding: 10,
+    width: "80%",
+    color: "white",
+    borderWidth: 2,
+    borderColor: "black",
+    borderRadius: 20,
+  },
+  addGoalContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-start",
     justifyContent: "center",
+    padding: 20,
+    paddingTop: 30,
+    width: "100%",
+    backgroundColor: "white",
+    color: "white",
+  },
+  addGoalButton: {
+    position: "absolute",
+    right: -5,
+    top: -10,
+    backgroundColor: "grey",
+    padding: 20,
+    borderRadius: 50,
+  },
+  buttonText: {
+    color: "white",
   },
 });
